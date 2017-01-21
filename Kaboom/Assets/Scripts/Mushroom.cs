114f391a60ae2eef8be0c387fd0c82592d3a6ca4
Yakka <emmanuel.corno@neuf.fr>
2017-01-21 19:02:32 +0100

    Une explosion... animée !

--- Kaboom/Assets/Scripts/Mushroom.cs
+++ Kaboom/Assets/Scripts/Mushroom.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Mushroom : MonoBehaviour {
+    private enum MushroomState {Counting = 0, Exploding, Length }
+    private MushroomState state;
     private SpriteRenderer sprite;
-
+    private float timer;
+    
     public void Start() {
         sprite = GetComponent<SpriteRenderer>();
         sprite.enabled = false;
+        timer = 0f;
+        state = MushroomState.Exploding;
+    }
+
+    public void Update() {
+        timer += Time.deltaTime;
+        if(timer > 2) {
+            Kaboom();
+        }
     }
 
     public void Kaboom() {
