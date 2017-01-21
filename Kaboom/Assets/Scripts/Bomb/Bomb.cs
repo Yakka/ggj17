b270979376be6efbe71443aa1bfcd170e5c5ed75
Yakka <emmanuel.corno@neuf.fr>
2017-01-21 12:38:20 +0100

    Nouvelle version du code des effets des ingrédients. Les ingrédients ont désormais des effets qui varient suivant leur quantité.

--- Kaboom/Assets/Scripts/Bomb/Bomb.cs
+++ Kaboom/Assets/Scripts/Bomb/Bomb.cs
@@ -5,24 +5,11 @@ using UnityEngine;
 public class Bomb : MonoBehaviour {
 
     public List<BombIngredient> bombIngredients = new List<BombIngredient>();
-
-    BombIngredient.IngredientEffects effects;
+    
 
     public void Start() {
         bombIngredients.AddRange(GetComponentsInChildren<BombIngredient>());
-        effects.effect1 = 0;
-        effects.effect2 = 0;
     }
     
-    public BombIngredient.IngredientEffects GetBombEffects() {
-        BombIngredient.IngredientEffects effects;
-        effects.effect1 = 0;
-        effects.effect2 = 0;
-        foreach(BombIngredient ingredient in bombIngredients) {
-            BombIngredient.IngredientEffects ingredientEffects = ingredient.GetIngredientEffects();
-            effects.effect1 += ingredientEffects.effect1;
-            effects.effect2 += ingredientEffects.effect2;
-        }
-        return effects;
-    }
+    
 }
--- Kaboom/Assets/Scripts/Bomb/BombIngredient.cs
+++ Kaboom/Assets/Scripts/Bomb/BombIngredient.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
+public enum EffectType {
+    Radiance,
+    Deflagration
+}
+
 public class BombIngredient : MonoBehaviour {
-    [System.Serializable]
-    public struct IngredientEffects {
-        public int effect1;
-        public int effect2;
-    }
 
     public string name; //Name of the component (UI)
     public string unit; //Unit name (UI)
     public int delta;   //Amount of this component per quantity (UI)
     public int min;     //Minimum value (gameplay)
     public int max;     //Maximum value (gameplay)
-    
-    public IngredientEffects unitEffects;
-
     private int quantity;   //Total amount of this component (gameplay)
+
+    [System.Serializable]
+    public struct Effect { public EffectType type; public int valuePerUnit; };
+    public List<Effect> effectsList = new List<Effect>(); //Used to set the game data
+
+    private Dictionary<EffectType, int> effectsDictionary = new Dictionary<EffectType, int>();
+
+    public void Start() {
+        foreach(Effect effect in effectsList) {
+            effectsDictionary.Add(effect.type, effect.valuePerUnit);
+        }
+    }
 
     public void SetGameplayQuantity(float _quantity) {
         quantity = Mathf.Clamp((int)_quantity, min, max);
@@ -32,10 +41,10 @@ public class BombIngredient : MonoBehaviour {
         return quantity * delta;
     }
 
-    public IngredientEffects GetIngredientEffects() {
-        IngredientEffects effects;
-        effects.effect1 = unitEffects.effect1 * quantity;
-        effects.effect2 = unitEffects.effect2 * quantity;
-        return effects;
+    public int GetFinalEffectValue(EffectType _type) {
+        int value = 0;
+        effectsDictionary.TryGetValue(_type, out value);
+        return value *= quantity;
     }
+
 }
