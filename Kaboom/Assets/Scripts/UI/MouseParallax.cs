0531235aa05c12eb2accb52faf21c6fdb795abe0
Loïc Huck <huck@lemonde.fr>
2017-01-21 23:34:34 +0100

    Integration graphisme de la création de bombe

---
+++ Kaboom/Assets/Scripts/UI/MouseParallax.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MouseParallax : MonoBehaviour {
+
+    public float scale = 2f;
+	Vector3 position;
+
+	void Start () {
+		position = transform.position;
+	}
+
+	void Update () {
+		float mouseX = (Input.mousePosition.x / Screen.width - 0.5f) * -scale * 0.1f;
+		float mouseY = (Input.mousePosition.y / Screen.width - 0.5f) * -scale * 0.1f;
+		transform.position = position + new Vector3(mouseX, mouseY, 0f);
+	}
+}
