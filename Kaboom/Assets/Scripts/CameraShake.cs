8331426f12393d08250181cd6ce4840148cecebc
Yakka <emmanuel.corno@neuf.fr>
2017-01-21 20:08:02 +0100

    Première version d'un screenshake.

---
+++ Kaboom/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    private float timer = 0f;
+    private float intensity;
+    private const float defaultIntensity = 0.1f;
+    private const float defaultTime = 0.5f;
+    private bool isShaking = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if(isShaking) {
+            timer -= Time.deltaTime;
+            if (timer > 0f) {
+                Camera.main.transform.Translate(Random.Range(-intensity, intensity), Random.Range(-intensity, intensity), 0f);
+            }
+            else {
+                Camera.main.transform.position = Vector3.back * 10;
+                isShaking = false;
+            }
+        }
+        
+        if(Input.anyKey) {
+            Shake();
+        }
+	}
+
+    public void Shake(float _time = defaultTime, float _intensity = defaultIntensity) {
+        timer = _time;
+        intensity = _intensity;
+        isShaking = true;
+    }
+}
