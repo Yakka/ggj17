2ec6412375b7aa3f796e8a2a563037327f540b94
Yakka <emmanuel.corno@neuf.fr>
2017-01-21 12:00:01 +0100

    La bombe est théoriquement capable de connaître ses effets. Code un peu cheum.

--- Kaboom/Assets/Scripts/Bomb/BombIngredient.cs
+++ Kaboom/Assets/Scripts/Bomb/BombIngredient.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class BombIngredient : MonoBehaviour {
+    [System.Serializable]
+    public struct IngredientEffects {
+        public int effect1;
+        public int effect2;
+    }
+
     public string name; //Name of the component (UI)
     public string unit; //Unit name (UI)
     public int delta;   //Amount of this component per quantity (UI)
     public int min;     //Minimum value (gameplay)
     public int max;     //Maximum value (gameplay)
+    
+    public IngredientEffects unitEffects;
 
     private int quantity;   //Total amount of this component (gameplay)
 
@@ -21,5 +30,12 @@ public class BombIngredient : MonoBehaviour {
 
     public int GetFullQuantity() {
         return quantity * delta;
+    }
+
+    public IngredientEffects GetIngredientEffects() {
+        IngredientEffects effects;
+        effects.effect1 = unitEffects.effect1 * quantity;
+        effects.effect2 = unitEffects.effect2 * quantity;
+        return effects;
     }
 }
