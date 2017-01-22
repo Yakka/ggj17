b222395046c6158a729a55bf23f63c984028d936
Loïc Huck <huck@lemonde.fr>
2017-01-22 14:34:04 +0100

    Destroy no bomb script

---
+++ Kaboom/Assets/Scripts/UI/DestroyIfNoBomb.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DestroyIfNoBomb : MonoBehaviour {
+
+	public bool destroyIfNoBomb = false;
+
+	void Start () {
+		bool noBomb = true;
+		foreach(BombIngredient ingredient in Bomb.instance.bombIngredients){
+			if(ingredient.GetGameplayQuantity() != 0f)
+				noBomb = false;
+		}
+
+		if(destroyIfNoBomb == noBomb)
+			Destroy(gameObject);
+	}
+}
